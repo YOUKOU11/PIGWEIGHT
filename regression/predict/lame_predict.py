05153aa4e012241a5a44d4d59b23528aaf0822b2
YOUKOU <616146880@qq.com>
2024-12-24 08:54:02 +0000

    删除文件 regression/predict/lame_predict.py

--- regression/predict/lame_predict.py
+++
@@ -1,9 +0,0 @@
-import torch
-from regression.dataloader import lame_dataloader
-
-
-def lame_predict(path):
-    X, true_weight = lame_dataloader.lame_dataloader_test(type='predict', path=path)
-    model = torch.load("../regression/model/best_lame_regression.pth")
-    Y_pred = model.predict(X)  # 对测试集数据，用predict函数预测
-    return Y_pred, true_weight
